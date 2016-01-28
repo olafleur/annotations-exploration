66ca68f959daec2a087fc73adc762e29a9bf6fb3
Olivier Lafleur <olivier.lafleur@gmail.com>
2016-01-27 22:56:47 -0500

    Generate code without Velocity

--- processors/src/main/java/com/olivierlafleur/guicetest/PatateProcessor.java
+++ processors/src/main/java/com/olivierlafleur/guicetest/PatateProcessor.java
@@ -6,8 +6,13 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.Set;
 
 @SupportedAnnotationTypes("com.olivierlafleur.guicetest.Patate")
@@ -18,6 +23,26 @@ public class PatateProcessor extends AbstractProcessor {
         for(Element element : roundEnv.getElementsAnnotatedWith(Patate.class)) {
             for(Element element1 : element.getEnclosedElements()) {
                 System.out.println("Truc : " + element1.getSimpleName());
+            }
+
+            if (element.getKind() == ElementKind.CLASS) {
+                TypeElement classElement = (TypeElement) element;
+                PackageElement packageElement = (PackageElement) element.getEnclosingElement();
+                try {
+                    JavaFileObject jfo = processingEnv.getFiler().createSourceFile(classElement.getQualifiedName() + "Gne");
+                    BufferedWriter bw = new BufferedWriter(jfo.openWriter());
+                    bw.append("package ");
+                    bw.append(packageElement.getQualifiedName());
+                    bw.append(";");
+                    bw.newLine();
+                    bw.newLine();
+                    bw.append("public class BillingServiceGne {}");
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
             }
         }
 
